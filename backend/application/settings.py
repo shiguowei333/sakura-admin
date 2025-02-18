09e2cf8030edd4f79c30866f78e1e518ee7b2c80
shi_guowei <shiguowei333@outlook.com>
2025-02-18 17:41:33 +0800

    安装drf和跨域配置app

--- backend/application/settings.py
+++ backend/application/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
