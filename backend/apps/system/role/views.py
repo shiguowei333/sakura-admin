2d114986780b0ec30921e6e0c6f1ae63051bd175
sakura3 <shiguowei333@outlook.com>
2025-03-04 21:46:01 +0800

    测试角色管理相关接口，去除分页查询

--- backend/apps/system/role/views.py
+++ backend/apps/system/role/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
+from utils.viewset import CustomViewSet
 from .models import Role
 from .serializers import RoleSerializer
 
 
 # Create your views here.
 
-class RoleViewSet(viewsets.ModelViewSet):
+class RoleViewSet(CustomViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
+    pagination_class = None
