b99c8fb078f5c13ec3e179062dcaf5d9de5d706d
shi_guowei <shiguowei333@outlook.com>
2025-03-07 18:16:19 +0800

    定义部门管理相关的api接口

---
+++ frondend/src/api/system/department.js
@@ -0,0 +1,11 @@
+import { http } from '@/utils/http'
+
+const url = '/system/department/'
+
+export const getDepartmentList = () => http.get(url)
+
+export const addDepartment = () => http.post(url)
+
+export const updateDepartment = (id, data) => http.put(url + id, data)
+
+export const deleteDepartment = (id) => http.delete(url + id)
