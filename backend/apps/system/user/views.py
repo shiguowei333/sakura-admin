1d293eef10c8c98987c289706afb3236c12dc1e7
shi_guowei <shiguowei333@outlook.com>
2025-03-18 17:18:19 +0800

    后端用户查询接口新增查询条件，增加返回字段，前端需要展示创建时间

--- backend/apps/system/user/views.py
+++ backend/apps/system/user/views.py
@@ -8,3 +8,12 @@ from .serializers import UserSerializer
 class UserViewSet(CustomViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        username = self.request.query_params.get('username', '')
+        telephone = self.request.query_params.get('telephone', '')
+        is_active = self.request.query_params.get('is_active', '')
+        self.queryset =  self.queryset.filter(username__icontains=username, telephone__icontains=telephone)
+        if is_active != '':
+            return self.queryset.filter(is_active=is_active)
+        return self.queryset
